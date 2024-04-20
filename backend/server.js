d9c1be89fef51a8f7f30939189f2aeb7cd4f4e77
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-04-20 12:34:15 +0200

    🎉 Start server whith Nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    
-
+    res.end('server is running again but with nodemon!!')
 });
+
+server.listen(process.env.PORT||3001)
+
